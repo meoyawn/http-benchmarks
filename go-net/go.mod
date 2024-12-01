2e3cf5dd202829c19177249d0c6153c2f32dfb74
adelnizamutdinov <stiggpwnz@gmail.com>
2024-12-01 16:14:39 +0300

    amen

---
+++ go-net/go.mod
@@ -0,0 +1,18 @@
+module adelnz.com/bench
+
+go 1.23.3
+
+require (
+	crawshaw.io/sqlite v0.3.2 // indirect
+	github.com/bahlo/generic-list-go v0.2.0 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
+	github.com/dprotaso/go-yit v0.0.0-20240618133044-5a0af90af097 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/pb33f/libopenapi v0.18.7 // indirect
+	github.com/pb33f/libopenapi-validator v0.2.2 // indirect
+	github.com/santhosh-tekuri/jsonschema/v6 v6.0.1 // indirect
+	github.com/vmware-labs/yaml-jsonpath v0.3.2 // indirect
+	github.com/wk8/go-ordered-map/v2 v2.1.9-0.20240815153524-6ea36470d1bd // indirect
+	golang.org/x/text v0.18.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- go-net/main.go
+++ go-net/main.go
@@ -1,17 +1,114 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"flag"
-	"fmt"
 	"net"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+
+	"crawshaw.io/sqlite/sqlitex"
+	"github.com/pb33f/libopenapi"
+	openapiValidator "github.com/pb33f/libopenapi-validator"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+var dbpool *sqlitex.Pool
+var validator openapiValidator.Validator = newValidator()
+
+type NewPost struct {
+	Content string `json:"content"`
+	Email   string `json:"email"`
+}
+
+// Helper function to handle error responses
+func respondJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(payload)
+}
+
+type Post struct {
+	ID        int64  `json:"id"`
+	UserID    int64  `json:"user_id"`
+	Content   string `json:"content"`
+	CreatedAt int64  `json:"created_at"`
+	UpdatedAt int64  `json:"updated_at"`
+}
+
+func handeNewPost(w http.ResponseWriter, req *http.Request) {
+	_, errors := validator.ValidateHttpRequest(req)
+	if len(errors) > 0 {
+		respondJSON(w, http.StatusBadRequest, errors)
+		return
+	}
+
+	var body NewPost
+	err := json.NewDecoder(req.Body).Decode(&body)
+	if err != nil {
+		respondJSON(w, http.StatusBadRequest, err)
+		return
+	}
+
+	writeConn := dbpool.Get(context.Background())
+
+	insertUser, err := writeConn.Prepare(`
+	INSERT OR IGNORE INTO users (email) VALUES (?);
+	`)
+	if err != nil {
+		respondJSON(w, http.StatusBadRequest, err)
+		return
+	}
+	insertPost, err := writeConn.Prepare(`
+	INSERT INTO posts   (content,   user_id)
+	SELECT              ?,          id
+	FROM        users
+	WHERE       email = ?
+	RETURNING   id, user_id, content, created_at, updated_at
+	`)
+	if err != nil {
+		respondJSON(w, http.StatusBadRequest, err)
+		return
+	}
+
+	sqlitex.Exec(writeConn, "BEGIN IMMEDIATE TRANSACTION", nil)
+	insertUser.BindText(1, body.Email)
+	insertUser.Step()
+
+	insertPost.BindText(1, body.Content)
+	insertPost.BindText(2, body.Email)
+	insertPost.Step()
+
+	var post Post = Post{
+		ID:        insertPost.ColumnInt64(1),
+		UserID:    insertPost.ColumnInt64(2),
+		Content:   insertPost.ColumnText(3),
+		CreatedAt: insertPost.ColumnInt64(4),
+		UpdatedAt: insertPost.ColumnInt64(5),
+	}
+	sqlitex.Exec(writeConn, "COMMIT", nil)
+
+	respondJSON(w, http.StatusCreated, post)
+}
+
+func newValidator() openapiValidator.Validator {
+	openApiJSON, err := os.ReadFile("../openapi/http.json")
+	if err != nil {
+		panic(err)
+	}
+
+	document, err := libopenapi.NewDocument(openApiJSON)
+	if err != nil {
+		panic(err)
+	}
+
+	v, errors := openapiValidator.NewValidator(document)
+	if len(errors) > 0 {
+		panic(errors)
+	}
+	return v
 }
 
 func main() {
@@ -20,8 +117,21 @@ func main() {
 	flag.StringVar(&socketFile, "socket", "/tmp/benchmark.sock", "Unix domain socket path. Default: /tmp/benchmark.sock")
 	flag.Parse()
 
-	srv := &http.Server{}
+	validator = newValidator()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /posts", handeNewPost)
+
+	server := &http.Server{
+		Handler: mux,
+	}
+
 	unixListener, err := net.Listen("unix", socketFile)
+	if err != nil {
+		panic(err)
+	}
+
+	dbpool, err = sqlitex.Open("file:../db/db.sqlite", 0, 10)
 	if err != nil {
 		panic(err)
 	}
@@ -30,8 +140,17 @@ func main() {
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
 		<-sigs
-		srv.Shutdown(nil)
+
+		err := server.Shutdown(context.TODO())
+		if err != nil {
+			panic(err)
+		}
+
+		err = dbpool.Close()
+		if err != nil {
+			panic(err)
+		}
 	}()
 
-	srv.Serve(unixListener)
+	server.Serve(unixListener)
 }
