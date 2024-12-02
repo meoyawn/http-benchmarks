aaaec036115888db58520ccb78e37d8e44e3c34e
adelnizamutdinov <stiggpwnz@gmail.com>
2024-12-02 14:14:20 +0300

    works

--- go-net/main.go
+++ go-net/main.go
@@ -1,33 +1,25 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
+	"errors"
 	"flag"
+	"fmt"
+	"github.com/eatonphil/gosqlite"
+	"github.com/pb33f/libopenapi"
+	validator "github.com/pb33f/libopenapi-validator"
+	"log"
 	"net"
 	"net/http"
 	"os"
 	"os/signal"
+	"reflect"
 	"syscall"
-
-	"crawshaw.io/sqlite/sqlitex"
-	"github.com/pb33f/libopenapi"
-	openapiValidator "github.com/pb33f/libopenapi-validator"
 )
 
-var dbpool *sqlitex.Pool
-var validator openapiValidator.Validator = newValidator()
-
 type NewPost struct {
-	Content string `json:"content"`
 	Email   string `json:"email"`
-}
-
-// Helper function to handle error responses
-func respondJSON(w http.ResponseWriter, code int, payload interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	json.NewEncoder(w).Encode(payload)
+	Content string `json:"content"`
 }
 
 type Post struct {
@@ -38,119 +30,197 @@ type Post struct {
 	UpdatedAt int64  `json:"updated_at"`
 }
 
-func handeNewPost(w http.ResponseWriter, req *http.Request) {
-	_, errors := validator.ValidateHttpRequest(req)
-	if len(errors) > 0 {
-		respondJSON(w, http.StatusBadRequest, errors)
-		return
-	}
+type PostResult struct {
+	ok  *Post
+	err *error
+}
 
-	var body NewPost
-	err := json.NewDecoder(req.Body).Decode(&body)
+func closeOrPanic(c interface{ Close() error }) {
+	log.Println("Closing", reflect.TypeOf(c))
+
+	err := c.Close()
 	if err != nil {
-		respondJSON(w, http.StatusBadRequest, err)
-		return
+		log.Panic(err)
 	}
+}
 
-	writeConn := dbpool.Get(context.Background())
+func dbWriter(requests chan NewPost, results chan PostResult) {
+	defer close(results)
 
-	insertUser, err := writeConn.Prepare(`
-	INSERT OR IGNORE INTO users (email) VALUES (?);
+	conn, err := gosqlite.Open("../db/db.sqlite")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer func() {
+		defer closeOrPanic(conn)
+
+		err := conn.Exec("PRAGMA optimize")
+		if err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	err = conn.Exec(`
+	PRAGMA journal_mode = wal;
+	PRAGMA synchronous = normal;
+	PRAGMA foreign_keys = on;
+	PRAGMA busy_timeout = 10000;
+	PRAGMA optimize=0x10002;
 	`)
 	if err != nil {
-		respondJSON(w, http.StatusBadRequest, err)
-		return
+		panic(err)
 	}
-	insertPost, err := writeConn.Prepare(`
+
+	insertUser, err := conn.Prepare("INSERT OR IGNORE INTO users (email) VALUES (?)")
+	if err != nil {
+		panic(err)
+	}
+	defer closeOrPanic(insertUser)
+
+	insertPost, err := conn.Prepare(`
 	INSERT INTO posts   (content,   user_id)
-	SELECT              ?,          id
+	SELECT              ?,          id  
 	FROM        users
 	WHERE       email = ?
 	RETURNING   id, user_id, content, created_at, updated_at
 	`)
 	if err != nil {
-		respondJSON(w, http.StatusBadRequest, err)
-		return
+		panic(err)
 	}
+	defer closeOrPanic(insertPost)
 
-	sqlitex.Exec(writeConn, "BEGIN IMMEDIATE TRANSACTION", nil)
-	insertUser.BindText(1, body.Email)
-	insertUser.Step()
+	for req := range requests {
+		res := Post{}
+		err = conn.WithTxImmediate(func() error {
+			err := insertUser.Exec(req.Email)
+			if err != nil {
+				return err
+			}
 
-	insertPost.BindText(1, body.Content)
-	insertPost.BindText(2, body.Email)
-	insertPost.Step()
+			err = insertPost.Bind(req.Content, req.Email)
+			if err != nil {
+				return err
+			}
+			ok, err := insertPost.Step()
+			if err != nil {
+				return err
+			}
+			if !ok {
+				return fmt.Errorf("no rows inserted %+v", req)
+			}
+			err = insertPost.Scan(&res.ID, &res.UserID, &res.Content, &res.CreatedAt, &res.UpdatedAt)
+			if err != nil {
+				return err
+			}
+			err = insertPost.Reset()
+			if err != nil {
+				return err
+			}
 
-	var post Post = Post{
-		ID:        insertPost.ColumnInt64(1),
-		UserID:    insertPost.ColumnInt64(2),
-		Content:   insertPost.ColumnText(3),
-		CreatedAt: insertPost.ColumnInt64(4),
-		UpdatedAt: insertPost.ColumnInt64(5),
+			return nil
+		})
+		if err != nil {
+			results <- PostResult{ok: nil, err: &err}
+		} else {
+			results <- PostResult{ok: &res, err: nil}
+		}
 	}
-	sqlitex.Exec(writeConn, "COMMIT", nil)
-
-	respondJSON(w, http.StatusCreated, post)
 }
 
-func newValidator() openapiValidator.Validator {
-	openApiJSON, err := os.ReadFile("../openapi/http.json")
-	if err != nil {
-		panic(err)
-	}
+func respondJSON(w http.ResponseWriter, status int, body interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
 
-	document, err := libopenapi.NewDocument(openApiJSON)
+	// write body json
+	err := json.NewEncoder(w).Encode(body)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
-
-	v, errors := openapiValidator.NewValidator(document)
-	if len(errors) > 0 {
-		panic(errors)
-	}
-	return v
 }
 
 func main() {
-
 	var socketFile string
 	flag.StringVar(&socketFile, "socket", "/tmp/benchmark.sock", "Unix domain socket path. Default: /tmp/benchmark.sock")
 	flag.Parse()
 
-	validator = newValidator()
+	requests := make(chan NewPost)
+	//defer close(requests) // closes dbWriter
+
+	results := make(chan PostResult) // closed by dbWriter since it's the sender
+
+	go dbWriter(requests, results)
+
+	openApiJSON, err := os.ReadFile("../openapi/http.json")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	document, err := libopenapi.NewDocument(openApiJSON)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	v, errs := validator.NewValidator(document)
+	if len(errs) > 0 {
+		log.Panic(errs)
+	}
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("POST /posts", handeNewPost)
+	mux.HandleFunc("POST /posts", func(w http.ResponseWriter, r *http.Request) {
+		_, errs := v.ValidateHttpRequest(r)
+		if len(errs) > 0 {
+			respondJSON(w, http.StatusBadRequest, errs)
+			return
+		}
+		var body NewPost
+		err := json.NewDecoder(r.Body).Decode(&body)
+		if err != nil {
+			respondJSON(w, http.StatusBadRequest, "Could not read body")
+			return
+		}
 
-	server := &http.Server{
-		Handler: mux,
-	}
+		requests <- body
+		result, ok := <-results
+		if !ok {
+			respondJSON(w, http.StatusInternalServerError, "Internal error")
+			return
+		}
+
+		if result.err != nil {
+			respondJSON(w, http.StatusInternalServerError, result.err)
+		} else {
+			respondJSON(w, http.StatusOK, result.ok)
+		}
+	})
 
 	unixListener, err := net.Listen("unix", socketFile)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 
-	dbpool, err = sqlitex.Open("file:../db/db.sqlite", 0, 10)
-	if err != nil {
-		panic(err)
+	server := http.Server{
+		Handler: mux,
 	}
 
 	sigs := make(chan os.Signal, 1)
+	defer close(sigs)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
-		<-sigs
+		sig := <-sigs
+		log.Println(sig)
 
-		err := server.Shutdown(context.TODO())
-		if err != nil {
-			panic(err)
-		}
+		close(requests) // closes dbWriter
 
-		err = dbpool.Close()
+		err := server.Close()
 		if err != nil {
-			panic(err)
+			log.Panic(err)
 		}
 	}()
 
-	server.Serve(unixListener)
+	log.Println(socketFile)
+
+	err = server.Serve(unixListener)
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
+		log.Panic(err)
+	}
 }
---
+++ go-net/main_test.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"errors"
+	"github.com/eatonphil/gosqlite"
+	"testing"
+)
+
+func testClose(t *testing.T, c interface{ Close() error }) {
+	err := c.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestGoSQLite(t *testing.T) {
+	conn, err := gosqlite.Open("../benchmarks/http/db/db.sqlite")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer testClose(t, conn)
+
+	err = conn.Exec(`
+	PRAGMA journal_mode = wal;
+	PRAGMA synchronous = normal;
+	PRAGMA foreign_keys = on;
+	PRAGMA busy_timeout = 10000;
+	`)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	insertUser, err := conn.Prepare("INSERT OR IGNORE INTO users (email) VALUES (?)")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer testClose(t, insertUser)
+
+	insertPost, err := conn.Prepare(`
+	INSERT INTO posts   (content,   user_id)
+	SELECT              ?,          id  
+	FROM        users
+	WHERE       email = ?
+	RETURNING   id, user_id, content, created_at, updated_at
+	`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer testClose(t, insertPost)
+
+	email := "foo@gmail.com"
+	content := "content"
+	post := Post{}
+	err = conn.WithTxImmediate(func() error {
+		err := insertUser.Exec(email)
+		if err != nil {
+			return err
+		}
+		err = insertPost.Bind(content, email)
+		if err != nil {
+			return err
+		}
+		ok, err := insertPost.Step()
+		if err != nil {
+			return err
+		}
+		if !ok {
+			return errors.New("no rows")
+		}
+		err = insertPost.Scan(&post.ID, &post.UserID, &post.Content, &post.CreatedAt, &post.UpdatedAt)
+		if err != nil {
+			return err
+		}
+		err = insertPost.Reset()
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if post.Content != content {
+		t.Fatal(post)
+	}
+}
