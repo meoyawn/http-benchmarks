12c2e4a25c6d316366f77631ed5646a1e3b816e9
adelnizamutdinov <stiggpwnz@gmail.com>
2024-12-05 17:56:11 +0300

    amen

--- rust-actix/src/main.rs
+++ rust-actix/src/main.rs
@@ -1,17 +1,9 @@
-use actix_web::{error, post, web, App, HttpResponse, HttpServer, Result};
-use rusqlite;
-use rusqlite::{CachedStatement, Connection, TransactionBehavior};
+use actix::prelude::*;
+use actix_web::{error, post, web, App, HttpResponse, HttpServer};
+use rusqlite::Connection;
 use serde::{Deserialize, Serialize};
 use std::io;
 use validator::Validate;
-
-#[derive(Debug, Validate, Deserialize)]
-struct NewPost {
-    #[validate(length(min = 1))]
-    content: String,
-    #[validate(email)]
-    email: String,
-}
 
 #[derive(Debug, Serialize)]
 struct Post {
@@ -22,10 +14,57 @@ struct Post {
     updated_at: i64,
 }
 
+#[derive(Debug, Validate, Deserialize)]
+struct NewPost {
+    #[validate(length(min = 1))]
+    content: String,
+    #[validate(email)]
+    email: String,
+}
+
+impl Message for &NewPost {
+    type Result = rusqlite::Result<Post>;
+}
+
+struct DbActor {
+    conn: Connection,
+}
+
+impl Actor for DbActor {
+    type Context = Context<Self>;
+
+    fn started(&mut self, _: &mut Context<Self>) {
+        self.conn
+            .execute_batch(
+                "
+                PRAGMA journal_mode = wal;
+                PRAGMA synchronous = normal;
+                PRAGMA foreign_keys = on;
+                PRAGMA busy_timeout = 10000;
+                
+                PRAGMA optimize = 0x10002;
+                ",
+            )
+            .unwrap();
+    }
+
+    fn stopped(&mut self, _: &mut Self::Context) {
+        self.conn.execute("PRAGMA optimize", []).unwrap();
+        // self.conn.close().unwrap()
+    }
+}
+
+impl Handler<&NewPost> for DbActor {
+    type Result = rusqlite::Result<Post>;
+
+    fn handle(&mut self, msg: &NewPost, _: &mut Self::Context) -> Self::Result {
+        transact(&self.conn, &msg)
+    }
+}
+
 #[post("/posts")]
-async fn handle_post(body: web::Json<NewPost>) -> Result<HttpResponse, actix_web::Error> {
-    body.validate()
-        .or_else(|e| Err(error::ErrorBadRequest(e)))?;
+async fn handle_post(body: web::Json<NewPost>) -> actix_web::Result<HttpResponse> {
+    body.validate().map_err(|e| error::ErrorBadRequest(e))?;
 
     let post = Post {
         id: 1,
@@ -38,82 +77,101 @@ async fn handle_post(body: web::Json<NewPost>) -> Result<HttpResponse, actix_web
     Ok(HttpResponse::Ok().json(post))
 }
 
+const INSERT_USER: &str = "INSERT OR IGNORE INTO users (email) VALUES (?1)";
+
+const INSERT_POST: &str = "
+INSERT INTO posts   (content,   user_id)
+SELECT              ?1,          id
+FROM        users
+WHERE       email = ?2
+RETURNING   id, user_id, content, created_at, updated_at
+";
+
+const OPEN_PRAGMAS: &str = "
+PRAGMA journal_mode = wal;
+PRAGMA synchronous = normal;
+PRAGMA foreign_keys = on;
+PRAGMA busy_timeout = 10000;
+
+PRAGMA optimize = 0x10002;
+";
+
+fn new_conn() -> rusqlite::Result<Connection> {
+    let conn = Connection::open("../db/db.sqlite")?;
+    conn.execute_batch(OPEN_PRAGMAS)?;
+    Ok(conn)
+}
+
 fn setup_db() -> rusqlite::Result<()> {
-    let mut conn = Connection::open("../db/db.sqlite")?;
+    let conn = new_conn()?;
 
-    conn.execute_batch(
-        "
-        PRAGMA journal_mode = wal;
-        PRAGMA synchronous = normal;
-        PRAGMA foreign_keys = on;
-        PRAGMA busy_timeout = 10000;
-        
-        PRAGMA optimize = 0x10002;
-        ",
-    )?;
-
-    let mut insert_user = conn.prepare_cached("INSERT OR IGNORE INTO users (email) VALUES (?1)")?;
-
-    let mut insert_post = conn.prepare_cached(
-        "
-        INSERT INTO posts   (content,   user_id)
-        SELECT              ?1,          id
-        FROM        users
-        WHERE       email = ?2
-        RETURNING   id, user_id, content, created_at, updated_at
-        ",
-    )?;
-
-    _ = transact(
-        &mut conn,
-        &mut insert_user,
-        &mut insert_post,
+    let post = transact(
+        &conn,
         &NewPost {
             content: String::from("content"),
             email: String::from("email@gmail.com"),
         },
     )?;
+    println!("{:?}", post);
 
     conn.execute("PRAGMA optimize", [])?;
 
     Ok(())
 }
 
-fn transact<'conn>(
-    conn: &'conn mut Connection,
-    insert_user: &'conn mut CachedStatement<'conn>,
-    insert_post: &'conn mut CachedStatement<'conn>,
-    np: &NewPost,
-) -> rusqlite::Result<Post> {
-    let tx = conn.transaction_with_behavior(TransactionBehavior::Immediate)?; // rolls back on error
+#[inline]
+fn immediate_tx<T, F>(conn: &Connection, exec: F) -> rusqlite::Result<T>
+where
+    F: FnOnce(&Connection) -> rusqlite::Result<T>,
+{
+    conn.prepare_cached("BEGIN IMMEDIATE TRANSACTION")?
+        .execute([])?;
 
-    insert_user.execute((&np.email,))?;
+    let ret = exec(conn);
 
-    let post = insert_post.query_row((&np.content, &np.email), |row| {
-        Ok(Post {
-            id: row.get(0)?,
-            user_id: row.get(1)?,
-            content: row.get(2)?,
-            created_at: row.get(3)?,
-            updated_at: row.get(4)?,
-        })
-    })?;
+    if ret.is_ok() {
+        conn.prepare_cached("COMMIT")?.execute([])?;
+    } else {
+        conn.prepare_cached("ROLLBACK")?.execute([])?;
+    }
 
-    tx.commit()?;
+    ret
+}
 
-    Ok(post)
+fn transact(conn: &Connection, np: &NewPost) -> rusqlite::Result<Post> {
+    immediate_tx(conn, |c| {
+        c.prepare_cached(INSERT_USER)?.execute([&np.email])?;
+
+        c.prepare_cached(INSERT_POST)?
+            .query_row([&np.content, &np.email], |row| {
+                Ok(Post {
+                    id: row.get(0)?,
+                    user_id: row.get(1)?,
+                    content: row.get(2)?,
+                    created_at: row.get(3)?,
+                    updated_at: row.get(4)?,
+                })
+            })
+    })
+}
+
+fn to_io(e: rusqlite::Error) -> io::Error {
+    io::Error::new(io::ErrorKind::Other, e)
 }
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
     let socket = "/tmp/benchmark.sock";
 
+    let conn = new_conn().map_err(to_io)?;
+
+    let addr = DbActor { conn }.start();
+
     println!("Listening on {}", socket);
 
-    HttpServer::new(|| App::new().service(handle_post))
-        .bind_uds(socket)?
-        .run()
-        .await
+    let https = HttpServer::new(move || App::new().app_data(addr.clone()).service(handle_post));
+    let srv = https.bind_uds(socket)?.run();
+    srv.await
 }
 
 #[cfg(test)]
