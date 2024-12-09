b9181310fd8fac9d8d5350431d774245ec7f15e2
adelnizamutdinov <stiggpwnz@gmail.com>
2024-12-09 03:20:11 +0300

    elixir is tedious

--- elixir_bandit/lib/application.ex
+++ elixir_bandit/lib/application.ex
@@ -1,3 +1,130 @@
+defmodule CachedConn do
+  @enforce_keys [:conn, :cache]
+  defstruct [:conn, :cache]
+
+  @type t :: %__MODULE__{
+          conn: Exqlite.Sqlite3.db(),
+          cache: %{String.t() => Exqlite.Sqlite3.statement()}
+        }
+
+  alias Exqlite.Sqlite3
+
+  @spec prepare(t(), String.t()) ::
+          {:ok, t(), Sqlite3.statement()} | {:error, Sqlite3.reason()}
+  def prepare(cc, sql) do
+    case Map.get(cc.cache, sql) do
+      nil ->
+        case Sqlite3.prepare(cc.conn, sql) do
+          {:ok, stmt} ->
+            {:ok, %{cc | cache: Map.put(cc.cache, sql, stmt)}, stmt}
+
+          {:error, reason} ->
+            {:error, reason}
+        end
+
+      stmt ->
+        {:ok, cc, stmt}
+    end
+  end
+
+  def immediate(cc, func) do
+    with {:ok, state, begin} <- prepare(cc, "BEGIN IMMEDIATE TRANSACTION"),
+         :done <- Sqlite3.step(state.conn, begin) do
+      with {:ok, state, result} <- func.(state),
+           {:ok, state, commit} <- prepare(state, "COMMIT"),
+           :done <- Sqlite3.step(state.conn, commit) do
+        {:ok, state, result}
+      else
+        {:error, reason} ->
+          with {:ok, state, rollback} <- prepare(cc, "ROLLBACK"),
+               :done <- Sqlite3.step(state.conn, rollback) do
+            {:error, reason}
+          else
+            {:error, reason} -> {:error, reason}
+          end
+      end
+    else
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+end
+
+defmodule DbWriter do
+  use GenServer
+
+  alias Exqlite.Sqlite3
+
+  @open_pragmas """
+  PRAGMA journal_mode = wal;
+  PRAGMA synchronous = normal;
+  PRAGMA foreign_keys = on;
+  PRAGMA busy_timeout = 10000;
+
+  PRAGMA optimize = 0x10002;
+  """
+
+  @insert_user "INSERT OR IGNORE INTO users (email) VALUES (?)"
+
+  @insert_post """
+  INSERT INTO posts   (content,   user_id)
+  SELECT              ?,          id
+  FROM        users
+  WHERE       email = ?
+  RETURNING   id, user_id, content, created_at, updated_at
+  """
+
+  @impl true
+  @spec init(path :: String.t()) :: {:ok, CachedConn.t()} | {:stop, Sqlite3.reason()}
+  def init(path) do
+    with {:ok, conn} <- Sqlite3.open(path),
+         :ok <- Sqlite3.execute(conn, @open_pragmas) do
+      {:ok, %CachedConn{conn: conn, cache: %{}}}
+    else
+      {:error, reason} -> {:stop, reason}
+    end
+  end
+
+  @impl true
+  def handle_call(request, _from, state) do
+    case CachedConn.immediate(state, fn state ->
+           with {:ok, state, iu} <- CachedConn.prepare(state, @insert_user),
+                :ok <- Sqlite3.bind(iu, [request.email]),
+                :done <- Sqlite3.step(state.conn, iu),
+                {:ok, state, ip} <- CachedConn.prepare(state, @insert_post),
+                :ok <- Sqlite3.bind(ip, [request.content, request.email]),
+                {:row, [id, user_id, content, created_at, updated_at]} <-
+                  Sqlite3.step(state.conn, ip) do
+             {:reply,
+              %{
+                id: id,
+                user_id: user_id,
+                content: content,
+                created_at: created_at,
+                updated_at: updated_at
+              }, state}
+           else
+             {:error, reason} -> {:reply, reason, state}
+           end
+         end) do
+      {:ok, state, result} -> {:reply, result, state}
+      {:error, reason} -> {:reply, reason, state}
+    end
+  end
+
+  @impl true
+  @spec terminate(Sqlite3.reason(), Sqlite3.db()) ::
+          :ok | {:error, Sqlite3.reason()}
+  def terminate(_reason, state) do
+    with :ok <- Sqlite3.execute(state, "PRAGMA optimize"),
+         :ok <- Sqlite3.close(state) do
+      :ok
+    else
+      {:error, reason} -> {:error, reason}
+    end
+  end
+end
+
 defmodule HttpPost do
   import Plug.Conn
 
@@ -27,8 +154,8 @@ defmodule HttpPost do
           updated_at: integer
         }
 
-  @spec on_valid(Plug.Conn.t(), new_post()) :: Plug.Conn.t()
-  def on_valid(conn, data) do
+  @spec on_valid(Plug.Conn.t(), data :: new_post()) :: Plug.Conn.t()
+  defp on_valid(conn, data) do
     conn
     |> put_resp_header("content-type", "application/json")
     |> send_resp(200, :json.encode(data))
